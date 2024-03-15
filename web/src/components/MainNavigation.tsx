f9ed244388bd3bb469159bdb0a18ff883d6dd032
Jakub Owczarek <jakub.owczarek@vip.onet.pl>
2024-03-15 11:58:46 +0100

    styling fixed

--- web/src/components/MainNavigation.tsx
+++ web/src/components/MainNavigation.tsx
@@ -1,9 +1,11 @@
 import { Link } from "react-router-dom";
+import styles from '../styles/MainNavigation.modules.css';
+
 
 const MainNavigation: React.FC<{}> = () => {
     return (
-        <header>
-            <nav>
+        <header >
+            <nav className={styles.cos}>
                 <ul>
                     <li><Link to="/users">Użytkownicy</Link></li>
                     <li><Link to="/user">Użytkownik</Link></li>
